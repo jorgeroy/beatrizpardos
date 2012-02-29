3356f5f9a1ca6c07a3d04e5988a09d8052f7d1b3
Jorge Roy <jorgeroy@gmail.com>
2012-02-29 19:14:09 +0100

    añadiendo libreria

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,9 +3,11 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//
-//= require jquery-1.7.1
+
 //= require jquery.nivo.slider
 //= require modernizr-1.7.min
+//= require shadowbox
+
+//= require jquery-1.7.1
 //= require_tree .
 
